77d4c3dcdc78faf6a834e02fc68133b166ab04a0
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-13 21:49:10 -0700

    Clean up App source

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+import React from 'react'
+import {
+	StyleSheet,
+	Text,
+	View
+} from 'react-native'
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		alignItems: 'center',
+		backgroundColor: '#fff',
+		flex: 1,
+		justifyContent: 'center'
+	}
+})
+
+// const App = () => {
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<Text>Hello from the Expo app!</Text>
+		</View>
+	)
+}
+
+// export { App }
+// export default App
