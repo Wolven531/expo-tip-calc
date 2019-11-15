2bdf510c8bbbf74c2e6a87dc91e1c2271c18414f
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-14 21:42:46 -0700

    Clean up MainTabNavigator source

--- navigation/MainTabNavigator.tsx
+++ navigation/MainTabNavigator.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Platform } from 'react-native'
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation'
+import {
+	createStackNavigator,
+	createBottomTabNavigator,
+	StackNavigatorConfig
+} from 'react-navigation'
 
 import TabBarIcon from '../components/TabBarIcon'
 import HomeScreen from '../screens/HomeScreen'
 import LinksScreen from '../screens/LinksScreen'
 import SettingsScreen from '../screens/SettingsScreen'
 
-const config = Platform.select({
-	default: {},
+const config: StackNavigatorConfig = Platform.select({
+	android: { headerMode: 'screen' },
+	default: { headerMode: 'screen' },
 	web: { headerMode: 'screen' }
 })
 
@@ -16,9 +21,10 @@ const HomeStack = createStackNavigator(
 	{
 		Home: HomeScreen,
 	},
-	config as any
-)
+	config
+);
 
+(HomeStack as any).path = ''
 HomeStack.navigationOptions = {
 	tabBarLabel: 'Home',
 	tabBarIcon: ({ focused }) => (
@@ -31,41 +37,37 @@ HomeStack.navigationOptions = {
 			}
 		/>
 	),
-};
-
-(HomeStack as any).path = ''
+}
 
 const LinksStack = createStackNavigator(
 	{
 		Links: LinksScreen,
 	},
-	config as any
-)
+	config
+);
 
+(LinksStack as any).path = ''
 LinksStack.navigationOptions = {
 	tabBarLabel: 'Links',
 	tabBarIcon: ({ focused }) => (
 		<TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
 	),
-};
-
-(LinksStack as any).path = ''
+}
 
 const SettingsStack = createStackNavigator(
 	{
 		Settings: SettingsScreen,
 	},
-	config as any
-)
+	config
+);
 
+(SettingsStack as any).path = ''
 SettingsStack.navigationOptions = {
 	tabBarLabel: 'Settings',
 	tabBarIcon: ({ focused }) => (
 		<TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
 	),
-};
-
-(SettingsStack as any).path = ''
+}
 
 const tabNavigator = createBottomTabNavigator({
 	HomeStack,
