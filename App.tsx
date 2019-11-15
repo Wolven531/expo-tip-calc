49139495123195a705857815ce2b0b88a8489958
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-14 21:43:40 -0700

    Don't load custom font

--- App.tsx
+++ App.tsx
@@ -1,16 +1,21 @@
 import { AppLoading } from 'expo'
 import { Asset } from 'expo-asset'
-import * as Font from 'expo-font'
+// import * as Font from 'expo-font'
 import React, { useState } from 'react'
-import { Platform, StatusBar, StyleSheet, View } from 'react-native'
-import { Ionicons } from '@expo/vector-icons'
+import {
+	// Platform,
+	// StatusBar,
+	StyleSheet,
+	View
+} from 'react-native'
+// import { Ionicons } from '@expo/vector-icons'
 
 import AppNavigator from './navigation/AppNavigator'
 
 const styles = StyleSheet.create({
 	container: {
 		alignItems: 'center',
-		backgroundColor: '#fff',
+		backgroundColor: '#333',
 		flex: 1,
 		justifyContent: 'center'
 	}
@@ -22,13 +27,13 @@ async function loadResourcesAsync() {
 			require('./assets/images/robot-dev.png'),
 			require('./assets/images/robot-prod.png'),
 		]),
-		Font.loadAsync({
-			// This is the font that we are using for our tab bar
-			...Ionicons.font,
-			// We include SpaceMono because we use it in HomeScreen.js. Feel free to
-			// remove this if you are not using it in your app
-			'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-		}),
+		// Font.loadAsync({
+		// 	// This is the font that we are using for our tab bar
+		// 	...Ionicons.font,
+		// 	// We include SpaceMono because we use it in HomeScreen.js. Feel free to
+		// 	// remove this if you are not using it in your app
+		// 	'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+		// }),
 	])
 }
 
@@ -47,6 +52,8 @@ export default function App(props) {
 	const [isLoadingComplete, setLoadingComplete] = useState(false)
 
 	if (!isLoadingComplete && !props.skipLoadingScreen) {
+	// if (!isLoadingComplete) {
+		// alert('loading!')
 		return (
 			<AppLoading
 				startAsync={loadResourcesAsync}
@@ -55,9 +62,10 @@ export default function App(props) {
 			/>
 		)
 	}
+	// alert('loaded!')
 	return (
 		<View style={styles.container}>
-			{Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+			{/* {Platform.OS === 'ios' && <StatusBar barStyle="default" />} */}
 			<AppNavigator />
 		</View>
 	)
