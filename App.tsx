5a857992c4684ee1a2965d769f656cba00f0beb4
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-14 21:20:41 -0700

    Load app resources using async; display AppLoading

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import {
-	StyleSheet,
-	Text,
-	View
-} from 'react-native'
+import { AppLoading } from 'expo'
+import { Asset } from 'expo-asset'
+import * as Font from 'expo-font'
+import React, { useState } from 'react'
+import { Platform, StatusBar, StyleSheet, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+
+import AppNavigator from './navigation/AppNavigator'
 
 const styles = StyleSheet.create({
 	container: {
@@ -14,11 +16,49 @@ const styles = StyleSheet.create({
 	}
 })
 
+async function loadResourcesAsync() {
+	await Promise.all([
+		Asset.loadAsync([
+			require('./assets/images/robot-dev.png'),
+			require('./assets/images/robot-prod.png'),
+		]),
+		Font.loadAsync({
+			// This is the font that we are using for our tab bar
+			...Ionicons.font,
+			// We include SpaceMono because we use it in HomeScreen.js. Feel free to
+			// remove this if you are not using it in your app
+			'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+		}),
+	])
+}
+
+function handleLoadingError(error) {
+	// In this case, you might want to report the error to your error reporting
+	// service, for example Sentry
+	console.warn(error)
+}
+
+function handleFinishLoading(setLoadingComplete) {
+	setLoadingComplete(true)
+}
+
 // const App = () => {
-export default function App() {
+export default function App(props) {
+	const [isLoadingComplete, setLoadingComplete] = useState(false)
+
+	if (!isLoadingComplete && !props.skipLoadingScreen) {
+		return (
+			<AppLoading
+				startAsync={loadResourcesAsync}
+				onError={handleLoadingError}
+				onFinish={() => handleFinishLoading(setLoadingComplete)}
+			/>
+		)
+	}
 	return (
 		<View style={styles.container}>
-			<Text>Hello from the Expo app!</Text>
+			{Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+			<AppNavigator />
 		</View>
 	)
 }
