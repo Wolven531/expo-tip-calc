67a4f5d6c1f079d3dc76491393ef5c6d3b7abe6d
Wolven531 <anthony.williams.cs@gmail.com>
2019-11-23 20:06:24 -0700

    Fix typing and exporting for SettingsScreen

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { ExpoConfigView } from '@expo/samples'
 
-export default function SettingsScreen() {
+const SettingsScreen: FC<any> = (props) => {
 	return <ExpoConfigView />
-}
+};
 
-SettingsScreen.navigationOptions = {
+(SettingsScreen as any).navigationOptions = {
 	title: 'app.json',
 }
+
+export { SettingsScreen }
