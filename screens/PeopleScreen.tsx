5ba3dcb91e17aec24d39848157e3b1ad9c7ee19b
Anthony Williams <anthony.williams.cs@gmail.com>
2019-11-25 22:15:59 -0700

    Add ability to save and load people

--- screens/PeopleScreen.tsx
+++ screens/PeopleScreen.tsx
@@ -1,5 +1,6 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import {
+	AsyncStorage,
 	Button,
 	FlatList,
 	ScrollView,
@@ -13,6 +14,31 @@ const PeopleScreen: FC<any> = (props) => {
 	const [newPersonName, setNewPersonName] = useState('')
 	const [people, setPeople] = useState<string[]>([])
 
+	const persistPeopleData = async () => {
+		try {
+			await AsyncStorage.setItem('expoTipCalc.people', JSON.stringify(people))
+		} catch (err) {
+			console.error('Problem saving people data', err)
+		}
+	}
+
+	const retrievePeopleData = async () => {
+		try {
+			const peopleStr = await AsyncStorage.getItem('expoTipCalc.people');
+			if (!peopleStr || peopleStr.length < 1) {
+				console.log('[retrievePeopleData] There was no people data, bailing...')
+				return
+			}
+			setPeople(JSON.parse(peopleStr))
+		} catch (err) {
+			console.error('Problem loading people data', err)
+		}
+	}
+
+	useEffect(() => {
+		retrievePeopleData()
+	}, [])
+
 	return (
 		<ScrollView style={styles.container}>
 			<Text style={styles.header}>People</Text>
@@ -21,15 +47,25 @@ const PeopleScreen: FC<any> = (props) => {
 				placeholder="New person name"
 				style={styles.inputNewPersonName}
 				value={newPersonName} />
-			<Button
-				onPress={() => {
-					if (newPersonName.length < 1) {
-						return
-					}
-					setPeople(stalePeople => stalePeople.concat(newPersonName))
-					setNewPersonName('')
-				}}
-				title="Add Person">Add Person</Button>
+			<View style={styles.buttonContainer}>
+				<View style={styles.buttonCell}>
+					<Button
+						onPress={() => {
+							if (newPersonName.length < 1) {
+								return
+							}
+							setPeople(stalePeople => stalePeople.concat(newPersonName))
+							setNewPersonName('')
+						}}
+						title="Add Person" />
+				</View>
+				<View style={styles.buttonCell}>
+					<Button
+						color="#3a3"
+						onPress={() => { persistPeopleData() }}
+						title="Save People" />
+				</View>
+			</View>
 			<View style={styles.peopleDisplay}>
 				{people.length < 1 && <Text style={{ textAlign: 'center' }}>There are no people currently</Text>}
 				<FlatList
@@ -47,6 +83,14 @@ const PeopleScreen: FC<any> = (props) => {
 }
 
 const styles = StyleSheet.create({
+	buttonCell: {
+		width: '40%'
+	},
+	buttonContainer: {
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+		width: '100%'
+	},
 	container: {
 		backgroundColor: '#fff',
 		flex: 1,
