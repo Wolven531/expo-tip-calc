66183810591a06dc33a495bdfe52f85b22986692
Wolven531 <anthony.williams.cs@gmail.com>
2019-11-26 21:04:00 -0700

    Load and display positions

--- screens/PositionsScreen.tsx
+++ screens/PositionsScreen.tsx
@@ -1,14 +1,32 @@
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import {
+	FlatList,
 	ScrollView,
 	StyleSheet,
-	Text
+	Text,
+	View
 } from 'react-native'
 
+import { retrievePositionsData } from '../services/PositionsService'
+
 const PositionsScreen: FC<any> = (props) => {
+	const [positions, setPositions] = useState<string[]>([])
+
+	useEffect(() => {
+		retrievePositionsData().then(loadedPositions => { setPositions(loadedPositions) })
+	}, [])
+
 	return (
 		<ScrollView style={styles.container}>
 			<Text style={styles.header}>Positions</Text>
+			<View style={styles.positionsDisplay}>
+				{positions.length < 1 && <Text style={{ textAlign: 'center' }}>There are no positions currently</Text>}
+				<FlatList
+					data={positions}
+					keyExtractor={(item: string, index: number) => String(index)}
+					renderItem={({ item }) => <Text>{item}</Text>}
+					/>
+			</View>
 		</ScrollView>
 	)
 }
@@ -26,6 +44,12 @@ const styles = StyleSheet.create({
 	header: {
 		fontSize: 16,
 		textAlign: 'center'
+	},
+	positionsDisplay: {
+		borderColor: '#333',
+		borderWidth: 1,
+		margin: 10,
+		padding: 10
 	}
 })
 
