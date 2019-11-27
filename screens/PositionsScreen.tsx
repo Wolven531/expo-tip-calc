211cced2575a3b755d5ae2903cc4be2fbc9347e6
Wolven531 <anthony.williams.cs@gmail.com>
2019-11-26 20:51:52 -0700

    Use styles and ScrollView

--- screens/PositionsScreen.tsx
+++ screens/PositionsScreen.tsx
@@ -1,16 +1,32 @@
 import React, { FC } from 'react'
-import { Text, View } from 'react-native'
+import {
+	ScrollView,
+	StyleSheet,
+	Text
+} from 'react-native'
 
 const PositionsScreen: FC<any> = (props) => {
 	return (
-		<View>
-			<Text>Positions</Text>
-		</View>
+		<ScrollView style={styles.container}>
+			<Text style={styles.header}>Positions</Text>
+		</ScrollView>
 	)
 }
 
 (PositionsScreen as any).navigationOptions = {
 	title: 'Positions'
 }
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#fff',
+		flex: 1,
+		padding: 15
+	},
+	header: {
+		fontSize: 16,
+		textAlign: 'center'
+	}
+})
 
 export { PositionsScreen }
