636c369e0b48a03fa1b85a1dd98b7fb5e1bf4790
Anthony Williams <anthony.williams.cs@gmail.com>
2019-12-02 21:01:29 -0700

    Add basic styling for RoleDisplay

--- components/RoleDisplay.tsx
+++ components/RoleDisplay.tsx
@@ -5,11 +5,11 @@ import {
 // 	FlatList,
 // 	Platform,
 // 	ScrollView,
-// 	StyleSheet,
+	StyleSheet,
 	Text,
 // 	TextInput,
 // 	TouchableOpacity,
-// 	View
+	View
 } from 'react-native'
 
 // import {
@@ -37,8 +37,33 @@ export interface IRoleDisplayProps {
 
 const RoleDisplay: FC<IRoleDisplayProps> = (props) => {
 	return (
-		<Text>{props.role.title} - {getPluralizedPoints(props.role.points)}</Text>
+		<View style={styles.container}>
+			<Text style={styles.fontTitle}>
+				{props.role.title}
+				<Text style={styles.fontSpacer}> - </Text>
+				<Text style={styles.fontValue}>{getPluralizedPoints(props.role.points)}</Text>
+			</Text>
+		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		borderColor: '#333',
+		borderRadius: 10,
+		borderWidth: 1,
+		marginVertical: 5,
+		padding: 10
+	},
+	fontSpacer: {
+		fontWeight: 'normal'
+	},
+	fontTitle: {
+		fontWeight: 'bold'
+	},
+	fontValue: {
+		fontWeight: 'normal'
+	}
+})
 
 export { RoleDisplay }
