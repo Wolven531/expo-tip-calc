d6674f9947d1eb9ed0740cf8faf8bbd4006607d6
Anthony Williams <anthony.williams.cs@gmail.com>
2019-12-02 21:49:00 -0700

    Use new DeleteButton in RoleDisplay

--- components/RoleDisplay.tsx
+++ components/RoleDisplay.tsx
@@ -1,22 +1,14 @@
 import React, { FC } from 'react'
 import {
-	Alert,
-	Platform,
 	StyleSheet,
 	Text,
-	TouchableOpacity,
 	View
 } from 'react-native'
 
-import {
-	LBL_DELETE_ROLE,
-	MSG_CANCEL,
-	MSG_CONFIRM_ROLE_DELETE,
-	MSG_OK,
-	TITLE_CONFIRM_ROLE_DELETE
-} from '../constants/Strings'
 import { Position } from '../models/Position'
 import { getPluralizedPoints } from '../services/utils'
+
+import { DeleteButton } from './DeleteButton'
 
 export interface IRoleDisplayProps {
 	onDelete: (role: Position) => void
@@ -31,69 +23,12 @@ const RoleDisplay: FC<IRoleDisplayProps> = (props) => {
 				<Text style={styles.fontSpacer}> - </Text>
 				<Text style={styles.fontValue}>{getPluralizedPoints(props.role.points)}</Text>
 			</Text>
-			<TouchableOpacity
-				onPress={() => {
-					Platform.select({
-						// android: () => {},
-						// ios: () => {}
-						default: () => {
-							Alert.alert(
-								TITLE_CONFIRM_ROLE_DELETE,
-								MSG_CONFIRM_ROLE_DELETE,
-								[
-									{
-										onPress: () => { },
-										style: 'cancel',
-										text: MSG_CANCEL
-									},
-									{
-										onPress: () => {
-											props.onDelete(props.role)
-										},
-										style: 'destructive',
-										text: MSG_OK
-									}
-								],
-								{ cancelable: false }
-							)
-						},
-						web: () => {
-							if (!confirm(MSG_CONFIRM_ROLE_DELETE)) {
-								return
-							}
-							props.onDelete(props.role)
-						}
-					})()
-				}}
-				style={styles.btnDelete}>
-				<Text style={styles.btnDeleteLabel}>{LBL_DELETE_ROLE}</Text>
-			</TouchableOpacity>
+			<DeleteButton onDelete={() => { props.onDelete(props.role) }} />
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
-	btnDelete: {
-		backgroundColor: '#f33',
-		borderRadius: 10,
-		padding: 10,
-		shadowColor: '#333',
-		shadowOffset: {
-			height: 3,
-			width: 2
-		},
-		shadowRadius: 2
-	},
-	btnDeleteLabel: {
-		color: '#ff0',
-		fontWeight: 'bold',
-		textShadowColor: '#333',
-		textShadowOffset: {
-			height: 3,
-			width: 2
-		},
-		textShadowRadius: 2
-	},
 	container: {
 		alignItems: 'center',
 		borderColor: '#333',
