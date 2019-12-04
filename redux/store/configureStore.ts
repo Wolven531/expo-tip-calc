6467e9d196b9211b5230282b391f2f869d3561c6
Wolven531 <anthony.williams.cs@gmail.com>
2019-12-03 20:34:35 -0700

    Add configureStore file for redux

---
+++ redux/store/configureStore.ts
@@ -0,0 +1,41 @@
+// import { connectRouter, routerMiddleware } from 'connected-react-router'
+// import { History } from 'history'
+import {
+	applyMiddleware,
+	combineReducers,
+	compose,
+	createStore
+} from 'redux'
+import thunk from 'redux-thunk'
+
+// import { IApplicationState, reducers } from './'
+import { rolesReducer } from '../reducers/rolesReducer'
+
+// const configureStore = (history: History, initialState?: any) => {
+const configureStore = (history: any, initialState?: any) => {
+	const middleware = [
+		thunk
+		// routerMiddleware(history)
+	]
+
+	const rootReducer = combineReducers({
+		rolesReducer
+		// ...reducers,
+		// router: connectRouter(history)
+	})
+
+	const enhancers: any[] = []
+	const windowIfDefined = typeof window === 'undefined' ? null : window as any
+
+	if (windowIfDefined && windowIfDefined.__REDUX_DEVTOOLS_EXTENSION__) {
+		enhancers.push(windowIfDefined.__REDUX_DEVTOOLS_EXTENSION__())
+	}
+
+	return createStore(
+		rootReducer,
+		initialState,
+		compose(applyMiddleware(...middleware), ...enhancers)
+	)
+}
+
+export { configureStore }
