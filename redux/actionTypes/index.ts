254f4af99c69bcdf824b6514396dee52a88d2819
Anthony Williams <anthony.williams.cs@gmail.com>
2019-12-09 23:52:34 -0700

    Add action types for redux person actions

--- redux/actionTypes/index.ts
+++ redux/actionTypes/index.ts
@@ -1,3 +1,9 @@
+// role section
 export const ADD_ROLE = 'ADD_ROLE'
 export const DELETE_ROLE = 'DELETE_ROLE'
 export const SET_ROLES = 'SET_ROLES'
+
+// person section
+export const ADD_PERSON = 'ADD_PERSON'
+export const DELETE_PERSON = 'DELETE_PERSON'
+export const SET_PEOPLE = 'SET_PEOPLE'
