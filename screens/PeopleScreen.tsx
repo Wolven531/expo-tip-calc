c8c114d57f6f97161211caae5f9e074debf7e1bb
Anthony Williams <anthony.williams.cs@gmail.com>
2019-12-11 23:03:11 -0700

    Attempt to align PeopleScreen w/ PositionsScreen

--- screens/PeopleScreen.tsx
+++ screens/PeopleScreen.tsx
@@ -9,6 +9,7 @@ import {
 	StyleSheet,
 	Text,
 	TextInput,
+	TouchableOpacity,
 	View
 } from 'react-native'
 import { connect } from 'react-redux'
@@ -23,8 +24,11 @@ import { IPeopleReducerProps } from '../redux/reducers/peopleReducer'
 import { IRolesReducerProps } from '../redux/reducers/rolesReducer'
 
 import {
+	LBL_ADD_NEW_PERSON,
 	MSG_OK,
 	MSG_PEOPLE_SAVED,
+	PLACEHOLDER_NEW_PERSON_NAME,
+	TITLE_ADD_PERSON,
 	TITLE_PEOPLE_SAVED
 } from '../constants/Strings'
 import { Person } from '../models/Person'
@@ -47,8 +51,11 @@ interface IPeopleScreenProps {
 }
 
 const PeopleScreenDC: FC<IPeopleScreenProps> = (props) => {
-	const [newPersonName, setNewPersonName] = useState('')
-	const [newPersonRoleIndex, setNewPersonRoleIndex] = useState(0)
+	const DEFAULT_NAME = ''
+	const DEFAULT_ROLE_INDEX = 0
+	const [newPersonName, setNewPersonName] = useState(DEFAULT_NAME)
+	const [newPersonRoleIndex, setNewPersonRoleIndex] = useState(DEFAULT_ROLE_INDEX)
+	const [isAddExpanded, setIsAddExpanded] = useState(false)
 
 	useEffect(() => {
 		retrievePeopleData()
@@ -60,63 +67,6 @@ const PeopleScreenDC: FC<IPeopleScreenProps> = (props) => {
 	return (
 		<ScrollView style={styles.container}>
 			<Text style={styles.header}>People</Text>
-			<View style={styles.newPersonContainer}>
-				<TextInput
-					onChangeText={text => setNewPersonName(text)}
-					placeholder="New person name"
-					style={styles.inputNewPersonName}
-					value={newPersonName} />
-				<Picker
-					onValueChange={(itemValue, itemIndex) => { setNewPersonRoleIndex(itemIndex) }}
-					selectedValue={''}
-					style={{ height: 35, width: '48%' }} >
-					{props.roles.map(role =>
-						<Picker.Item
-							key={role.title}
-							label={`${role.title} (${role.points} pt${role.points > 1 ? 's' : ''})`}
-							value={role.title.toLowerCase()} />
-					)}
-				</Picker>
-			</View>
-			<View style={styles.buttonContainer}>
-				<View style={styles.buttonCell}>
-					<Button
-						onPress={() => {
-							if (newPersonName.length < 1) {
-								return
-							}
-							props.addPerson(new Person(newPersonName, props.roles[newPersonRoleIndex]))
-							setNewPersonName('')
-						}}
-						title="Add Person" />
-				</View>
-				<View style={styles.buttonCell}>
-					<Button
-						color="#3a3"
-						onPress={() => {
-							persistPeopleData(props.people)
-							Platform.select({
-								default: () => {
-									Alert.alert(
-										TITLE_PEOPLE_SAVED,
-										MSG_PEOPLE_SAVED,
-										[
-											{
-												onPress: () => { },
-												text: MSG_OK
-											}
-										],
-										{ cancelable: false }
-									)
-								},
-								web: () => {
-									alert(MSG_PEOPLE_SAVED)
-								}
-							})()
-						}}
-						title="Save People" />
-				</View>
-			</View>
 			<View style={styles.peopleDisplay}>
 				{props.people.length < 1 && <Text style={{ textAlign: 'center' }}>There are no people currently</Text>}
 				<FlatList
@@ -127,6 +77,69 @@ const PeopleScreenDC: FC<IPeopleScreenProps> = (props) => {
 							onDelete={() => { props.deletePerson(item) }}
 						 	person={item} />}
 					/>
+				<View style={styles.newPersonContainer}>
+					<TextInput
+						onChangeText={text => setNewPersonName(text)}
+						placeholder="New person name"
+						style={styles.inputNewPersonName}
+						value={newPersonName} />
+					<Picker
+						onValueChange={(itemValue, itemIndex) => { setNewPersonRoleIndex(itemIndex) }}
+						selectedValue={''}
+						style={{ height: 35, width: '48%' }} >
+						{props.roles.map(role =>
+							<Picker.Item
+								key={role.title}
+								label={`${role.title} (${role.points} pt${role.points > 1 ? 's' : ''})`}
+								value={role.title.toLowerCase()} />
+						)}
+					</Picker>
+					<TouchableOpacity
+						onPress={() => setIsAddExpanded(staleExpanded => !staleExpanded)}
+						style={styles.buttonNewPositionExpander}>
+						<Text style={styles.expanderText}>{isAddExpanded ? '-' : '+'} {LBL_ADD_NEW_PERSON}</Text>
+					</TouchableOpacity>
+					{isAddExpanded && <View>
+						<View style={styles.newPersonInputContainer}>
+							<TextInput
+								onChangeText={setNewPersonName}
+								placeholder={PLACEHOLDER_NEW_PERSON_NAME}
+								style={styles.inputNewPersonName}
+								value={newPersonName} />
+						</View>
+						<Button
+							onPress={() => {
+								props.addPerson(new Person(newPersonName, props.roles[newPersonRoleIndex]))
+								setNewPersonName(DEFAULT_NAME)
+								setNewPersonRoleIndex(DEFAULT_ROLE_INDEX)
+							}}
+							title={TITLE_ADD_PERSON} />
+					</View>}
+				</View>
+				<Button
+					color="#3a3"
+					onPress={() => {
+						persistPeopleData(props.people)
+						Platform.select({
+							default: () => {
+								Alert.alert(
+									TITLE_PEOPLE_SAVED,
+									MSG_PEOPLE_SAVED,
+									[
+										{
+											onPress: () => { },
+											text: MSG_OK
+										}
+									],
+									{ cancelable: false }
+								)
+							},
+							web: () => {
+								alert(MSG_PEOPLE_SAVED)
+							}
+						})()
+					}}
+					title="Save People" />
 			</View>
 		</ScrollView>
 	)
@@ -145,10 +158,15 @@ const styles = StyleSheet.create({
 		justifyContent: 'space-between',
 		width: '100%'
 	},
+	buttonNewPositionExpander: { },
 	container: {
 		backgroundColor: '#fff',
 		flex: 1,
 		padding: 15
+	},
+	expanderText: {
+		fontSize: 16,
+		fontWeight: 'bold'
 	},
 	header: {
 		fontSize: 16,
@@ -158,9 +176,10 @@ const styles = StyleSheet.create({
 		borderColor: '#333',
 		borderWidth: 1,
 		height: 35,
+		marginTop: 10,
 		paddingHorizontal: 5,
 		paddingVertical: 2,
-		width: '48%'
+		width: '80%'
 	},
 	newPersonContainer: {
 		borderColor: '#333',
@@ -170,6 +189,12 @@ const styles = StyleSheet.create({
 		marginBottom: 10,
 		marginTop: 10,
 		padding: 5,
+		width: '100%'
+	},
+	newPersonInputContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginBottom: 10,
 		width: '100%'
 	},
 	peopleDisplay: {
