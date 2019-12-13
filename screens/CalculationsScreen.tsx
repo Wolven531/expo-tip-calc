5925cff7bcf696f4e1181a05f05cec916a82acb6
Wolven531 <anthony.williams.cs@gmail.com>
2019-12-12 21:45:35 -0700

    Add CalculationsScreen

---
+++ screens/CalculationsScreen.tsx
@@ -0,0 +1,50 @@
+import React, { FC } from 'react'
+import {
+	Platform,
+	StyleSheet,
+	Text,
+	View
+} from 'react-native'
+
+const CalculationsScreen: FC<any> = (props) => {
+	return (
+		<View style={styles.container}>
+			<View style={styles.tabBarInfoContainer}>
+				<Text>Hello</Text>
+			</View>
+		</View>
+	)
+};
+
+(CalculationsScreen as any).navigationOptions = {
+	header: null
+}
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#fff',
+		flex: 1
+	},
+	tabBarInfoContainer: {
+		alignItems: 'center',
+		backgroundColor: '#fbfbfb',
+		bottom: 0,
+		left: 0,
+		paddingVertical: 20,
+		position: 'absolute',
+		right: 0,
+		...Platform.select({
+			android: {
+				elevation: 20,
+			},
+			ios: {
+				shadowColor: 'black',
+				shadowOffset: { width: 0, height: -3 },
+				shadowOpacity: 0.1,
+				shadowRadius: 3,
+			}
+		})
+	}
+})
+
+export { CalculationsScreen }
