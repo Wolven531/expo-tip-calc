6d1afa5f26e0736da02772ab0382717877b84a78
Anthony Williams <anthony.williams.cs@gmail.com>
2019-12-14 23:13:16 -0700

    First pass at using multi select

--- screens/CalculationsScreen.tsx
+++ screens/CalculationsScreen.tsx
@@ -7,12 +7,13 @@ import {
 } from 'react-native'
 // NOTE: requires more work: https://github.com/react-native-community/react-native-datetimepicker
 // import DateTimePicker from '@react-native-community/datetimepicker'
+import CustomMultiPicker from 'react-native-multiple-select-list'
 
 const CalculationsScreen: FC<any> = (props) => {
 	return (
 		<View style={styles.container}>
 			<View style={styles.tabBarInfoContainer}>
-				<Text>Hello</Text>
+
 				{/*
 				<DateTimePicker
 					value={new Date()}
@@ -22,6 +23,31 @@ const CalculationsScreen: FC<any> = (props) => {
 					onChange={() => { return }}
 					/>
 				*/}
+				<CustomMultiPicker
+					callback={(res) => {
+						console.log(res)
+					}} // callback, array of selected items
+					iconColor={'#00a2dd'}
+					iconSize={20}
+					multiple={true} //
+					// options={userList}
+					options={['amy', 'bob']}
+					// placeholder={'Search'}
+					// placeholderTextColor={'#757575'}
+					returnValue={'label'} // 'label' | 'value'
+					rowBackgroundColor={'#eee'}
+					rowHeight={50}
+					rowRadius={15}
+					scrollViewHeight={150}
+					// search={true} // show search bar
+					selectedIconName={Platform.OS === 'ios'
+						? 'ios-checkmark-circle-outline'
+						: 'md-checkmark-circle-outline'}
+					unselectedIconName={Platform.OS === 'ios'
+						? 'ios-radio-button-off'
+						: 'md-radio-button-off'}
+					// selected={[1,2]} // list of options which are selected by default
+				/>
 			</View>
 		</View>
 	)
