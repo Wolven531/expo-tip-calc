ae58dc3cf143e612707cf8d06d29963feeaa17a3
Anthony Williams <anthony.williams.cs@gmail.com>
2019-12-16 20:46:21 -0700

    Restyle calc screen

--- screens/CalculationsScreen.tsx
+++ screens/CalculationsScreen.tsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import {
 	Platform,
 	StyleSheet,
-	// Text,
+	Text,
 	View
 } from 'react-native'
 // NOTE: requires more work: https://github.com/react-native-community/react-native-datetimepicker
@@ -37,50 +37,43 @@ const CalculationsScreenDC: FC<ICalculationsScreenProps> = (props) => {
 
 	return (
 		<View style={styles.container}>
-			<View style={styles.tabBarInfoContainer}>
-
-				{/*
-				<DateTimePicker
-					value={new Date()}
-					mode={'date'} // 'date' | ???
-					is24Hour={false}
-					display={'default'} // 'calendar' | 'clock' | 'spinner'
-					onChange={() => { return }}
-					/>
-				*/}
-				<CustomMultiPicker
-					callback={(res: any[]) => {
-						// if (res.length === 0) { // one value means everything is deselected
-						// 	setSelectedPeopleIndices([])
-						// 	return
-						// }
-
-						setSelectedPeopleIndices(res)
-					}} // callback, array of selected items
-					iconColor={'#00a2dd'}
-					iconSize={20}
-					multiple={true} //
-					// options={userList}
-					options={props.people.map(person => person.name)}
-					// placeholder={'Search'}
-					// placeholderTextColor={'#757575'}
-					// returnValue={'label'} // 'label' | 'value'
-					returnValue={'value'} // 'label' | 'value'
-					rowBackgroundColor={'#eee'}
-					rowHeight={50}
-					rowRadius={15}
-					// scrollViewHeight={150}
-					// search={true} // show search bar
-					selectedIconName={Platform.OS === 'ios'
-						? 'ios-checkmark-circle-outline'
-						: 'md-checkmark-circle-outline'}
-					unselectedIconName={Platform.OS === 'ios'
-						? 'ios-radio-button-off'
-						: 'md-radio-button-off'}
-					// selected={[1,2]} // list of options which are selected by default
-					selected={selectedPeopleIndices}
-				/>
+			<View style={styles.headerTextSelectPeople}>
+				<Text style={styles.headerText}>Select Workers</Text>
 			</View>
+			{/*
+			<DateTimePicker
+				value={new Date()}
+				mode={'date'} // 'date' | ???
+				is24Hour={false}
+				display={'default'} // 'calendar' | 'clock' | 'spinner'
+				onChange={() => { return }}
+				/>
+			*/}
+			<CustomMultiPicker
+				callback={setSelectedPeopleIndices} // callback, array of selected items
+				iconColor={'#00a2dd'}
+				iconSize={20}
+				multiple={true} //
+				// options={userList}
+				options={props.people.map(person => person.name)}
+				// placeholder={'Search'}
+				// placeholderTextColor={'#757575'}
+				// returnValue={'label'} // 'label' | 'value'
+				returnValue={'value'} // 'label' | 'value'
+				rowBackgroundColor={'#eee'}
+				rowHeight={50}
+				rowRadius={15}
+				// scrollViewHeight={150}
+				// search={true} // show search bar
+				selectedIconName={Platform.OS === 'ios'
+					? 'ios-checkmark-circle-outline'
+					: 'md-checkmark-circle-outline'}
+				unselectedIconName={Platform.OS === 'ios'
+					? 'ios-radio-button-off'
+					: 'md-radio-button-off'}
+				// selected={[1,2]} // list of options which are selected by default
+				selected={selectedPeopleIndices}
+			/>
 		</View>
 	)
 };
@@ -94,25 +87,13 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		flex: 1
 	},
-	tabBarInfoContainer: {
-		alignItems: 'center',
-		backgroundColor: '#fbfbfb',
-		bottom: 0,
-		left: 0,
-		paddingVertical: 20,
-		position: 'absolute',
-		right: 0,
-		...Platform.select({
-			android: {
-				elevation: 20,
-			},
-			ios: {
-				shadowColor: 'black',
-				shadowOffset: { width: 0, height: -3 },
-				shadowOpacity: 0.1,
-				shadowRadius: 3,
-			}
-		})
+	headerText: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		textAlign: 'center'
+	},
+	headerTextSelectPeople: {
+		padding: 15
 	}
 })
 
