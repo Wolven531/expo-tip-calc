8a75259828590db2f30286fa8e164aa65d08a724
Anthony Williams <anthony.williams.cs@gmail.com>
2019-12-28 18:35:31 -0700

    Start work on breakdown display and props

--- components/TipBreakdown.tsx
+++ components/TipBreakdown.tsx
@@ -7,16 +7,39 @@ import {
 
 // constants
 import {
-	HEADER_BREAKDOWN
+	HEADER_BREAKDOWN,
+	METHOD_COMMUNIST,
+	METHOD_HOUR_WEIGHTED,
+	METHOD_ROLE_CENTRIC
 } from '../constants/Strings'
 
 import { Person } from '../models/Person'
 
-const TipBreakdown: FC<any> = (props) => {
+interface ITipBreakdownProps {
+	calculationMethod: string // 'Communist' | 'Hour Weighted' | 'Role-centric'
+	people: Person[]
+	totalTip: number
+}
+
+const TipBreakdown: FC<ITipBreakdownProps> = (props) => {
+	let totalPerPerson = 0
+
+	switch (props.calculationMethod) {
+		case METHOD_COMMUNIST:
+			totalPerPerson = props.totalTip / props.people.length
+			break;
+		case METHOD_HOUR_WEIGHTED:
+
+			break;
+		case METHOD_ROLE_CENTRIC:
+
+			break;
+	}
 
 	return (
 		<View style={styles.container}>
 			<Text style={styles.headerText}>{HEADER_BREAKDOWN}</Text>
+			<Text style={{}}>Total Per Person: {totalPerPerson}</Text>
 		</View>
 	)
 }
