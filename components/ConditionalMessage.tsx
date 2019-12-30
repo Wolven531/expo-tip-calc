b3b0a00daf46985b9e8c93205781fae3547ba978
Anthony Williams <anthony.williams.cs@gmail.com>
2019-12-29 20:59:05 -0700

    Display ConditionalMessage children

--- components/ConditionalMessage.tsx
+++ components/ConditionalMessage.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo, useState } from 'react'
+import React, { FC, memo } from 'react'
 import {
 	StyleSheet,
 	Text,
@@ -7,20 +7,39 @@ import {
 
 interface IConditionalMessageProps {
 	collection: any[] | null | undefined
-	containerStyle: any | undefined
 	message: string
+	styleContainer: any | undefined
+	styleText: any | undefined
 }
 
+/**
+ * @summary Component used to display one of two things: a message or its children
+ * @description If the collection provided is null, undefined, or has a length of zero (0),
+ *   the message provided will be displayed; otherwise, the children provided to this
+ *   component will be displayed
+ * @param collection @type any[] | null | undefined
+ * @param message @type string
+ * @param styleContainer @type any | undefined
+ * @param styleText @type any | undefined
+ */
 const ConditionalMessage: FC<IConditionalMessageProps> = (props) => {
-	return (
-		<View style={props.containerStyle ? props.containerStyle : styles.container}>
+	const shouldDisplayMessage = props.collection === null
+		|| props.collection === undefined
+		|| props.collection.length === 0
 
+	return (
+		<View style={props.styleContainer ? props.styleContainer : styles.container}>
+			{shouldDisplayMessage && <Text style={props.styleText ? props.styleText : styles.text}>{props.message}</Text>}
+			{!shouldDisplayMessage && props.children}
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
 	container: {
+
+	},
+	text: {
 
 	}
 })
