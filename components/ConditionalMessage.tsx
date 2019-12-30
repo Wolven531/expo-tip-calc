9ec28917aff7159a5473a88cd578824ff6916844
Anthony Williams <anthony.williams.cs@gmail.com>
2019-12-29 20:44:57 -0700

    Start work on ConditionalMessage

---
+++ components/ConditionalMessage.tsx
@@ -0,0 +1,13 @@
+import React, { FC, memo, useState } from 'react'
+import {
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View
+} from 'react-native'
+
+const ConditionalMessage: FC<any> = (props) => {
+	return ( null )
+}
+
+export { ConditionalMessage }
