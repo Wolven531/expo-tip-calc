655ce499b3f284b57bb15360cd253cdfe1e7b919
Anthony Williams <anthony.williams.cs@gmail.com>
2019-12-29 20:51:19 -0700

    Add interface for ConditionalMessage

--- components/ConditionalMessage.tsx
+++ components/ConditionalMessage.tsx
@@ -2,12 +2,27 @@ import React, { FC, memo, useState } from 'react'
 import {
 	StyleSheet,
 	Text,
-	TouchableOpacity,
 	View
 } from 'react-native'
 
-const ConditionalMessage: FC<any> = (props) => {
-	return ( null )
+interface IConditionalMessageProps {
+	collection: any[] | null | undefined
+	containerStyle: any | undefined
+	message: string
 }
+
+const ConditionalMessage: FC<IConditionalMessageProps> = (props) => {
+	return (
+		<View style={props.containerStyle ? props.containerStyle : styles.container}>
+
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+
+	}
+})
 
 export { ConditionalMessage }
