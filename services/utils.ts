d6d3972e74bff83d07a2c2c54e507f2b8a7cb4c4
Anthony Williams <anthony.williams.cs@gmail.com>
2019-12-30 20:43:02 -0700

    Extract prettifyMoney into utils

--- services/utils.ts
+++ services/utils.ts
@@ -1,7 +1,12 @@
+import Intl from 'intl'
+require('intl/locale-data/jsonp/en.js')
+
 import {
 	LBL_POINTS_MULTIPLE,
 	LBL_POINTS_SINGLE
 } from '../constants/Strings'
+
+const usdFormatter = new Intl.NumberFormat('en-US', { currency: 'USD', style: 'currency' })
 
 const getPluralizedPoints = (points: number): string => {
 	switch (points) {
@@ -14,9 +19,16 @@ const getPluralizedPoints = (points: number): string => {
 	}
 }
 
+const prettifyMoney = (numStr: string) => {
+	const num = parseFloat(numStr)
+
+	return usdFormatter.format(num)
+}
+
 const simpleNumberSort = (a: number, b: number) => a < b ? -1 : (a > b ? 1 : 0)
 
 export {
 	getPluralizedPoints,
+	prettifyMoney,
 	simpleNumberSort
 }
